e1a09be8822493ad512be7eee5e16b651cfcd532
Andrea Seminario <andrea.ingenieriaindustrial@gmail.com>
2022-06-29 22:48:49 -0300

    Primer funcion: from head import prepresentacion

---
+++ ProyectoIntegrador/A13G5.py
@@ -0,0 +1,9 @@
+"""                 PROYECTO INTEGRADOR - SITUACION PROFESIONALIZANTE 1
+                                AULA 13 - GRUPO 5
+"""
+# Importacion de modulos
+
+import head as h
+
+# Llamamos a las funciones
+h.presentacion()
