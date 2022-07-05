57367f7616e7e217500869b3c2f053b187251a43
daianne <daianasenese@gmail.com>
2022-07-05 11:36:22 -0300

    Agrego las primeras funciones en Python

---
+++ A13G5.py
@@ -0,0 +1,15 @@
+#Esta aplicación debe llamar a funciones, cada una en su archivo .py a saber:
+#función que muestre cartel de presentación.
+#función ing2i, debe permitir el ingreso de 2 valores enteros
+#función ing2s, debe permitir el ingreso de 2 valores string
+#1- función suma, retorna la suma de 2 parámetros.
+
+print ("Bienvenidos al grupo 5 del aula 13")
+ing2i1 = int (input ("Ingrese un número "))
+ing2i2 = int (input ("Ingrese un segundo número "))
+
+ing2s1 = input ("Ingrese un string ")
+ing2s2 = input ("Ingrese un segundo string ")
+
+print ("La suma de ambos números da como resultado: " , ing2i1 + ing2i2)
+
